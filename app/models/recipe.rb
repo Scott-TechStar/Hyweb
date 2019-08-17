0af3e71d465f2eb22438d388e2a148b917ea9a1b
Waithira26 <waithira99irungu@gmail.com>
2019-08-17 22:19:21 +0300

    created all relevant associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,6 @@ class Recipe < ApplicationRecord
   has_many :steps, dependent: :destroy
   accepts_nested_attributes_for :steps, allow_destroy: true
 
+  belongs_to :category
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_165151) do
+ActiveRecord::Schema.define(version: 2019_08_17_185341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_165151) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "steps", force: :cascade do |t|
