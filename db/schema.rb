076e032284e500e024a03abbe536f2ae53c07bca
Waithira26 <waithira99irungu@gmail.com>
2019-08-17 19:18:40 +0300

    removed unwanted scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_095741) do
+ActiveRecord::Schema.define(version: 2019_07_13_094101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2019_07_13_095741) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "category_id"  
   end
 
   create_table "steps", force: :cascade do |t|
