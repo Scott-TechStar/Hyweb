68e5f17662c0d830868370986a38cbeb2247a033
Waithira26 <waithira99irungu@gmail.com>
2019-08-17 20:53:41 +0300

    added new category controller and model

--- db/migrate/20190713095741_add_category_id_to_recipe.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryIdToRecipe < ActiveRecord::Migration[5.2]
-  def change
-    add_column :recipes, :category_id, :bigint
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_094101) do
+ActiveRecord::Schema.define(version: 2019_08_17_165151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_07_13_094101) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
