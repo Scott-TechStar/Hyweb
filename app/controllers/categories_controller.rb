412a01423389e0aef4e50f2190e6affa09a625e6
katungi <dankatdennis@gmail.com>
2019-08-22 10:03:05 +0300

    added category ui and actions in the controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,29 @@ class CategoriesController < ApplicationController
     @categories  = Category.all
   end
 
+  def new
+    @category = Category.new()
+  end
+
+  def create
+
+    @category = Category.new(category_params)
+
+    if @category.save
+
+      render 'index'
+
+    else
+      render 'new'
+    end
+  end
+end
+
+
+private
+
+def category_params
+
+  params.require(:category).permit(:name,:description)
+
 end
